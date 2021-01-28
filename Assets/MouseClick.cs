22f3090c8e43feae06ffa16f50b87e8ba5add9a9
KazuhitoTakahashi <shigeki.takahashi.dev@gmail.com>
2021-01-29 01:36:32 +0900

    Initial commit

---
+++ Assets/MouseClick.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseClick : MonoBehaviour
+{
+	GameObject referenceObj;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		referenceObj = GameObject.Find("Plane");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) {
+            VideoLoader videoloader = referenceObj.GetComponent<VideoLoader>();
+            videoloader.VideoPlayerControl();
+        }
+    }
+}
---
+++ Assets/VideoLoader.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.Video;
+
+[RequireComponent(typeof(VideoPlayer))]
+public class VideoLoader : MonoBehaviour 
+{
+
+    [SerializeField]
+    private string relativePath;
+
+	// Use this for initialization
+    void Start()
+    {
+        VideoPlayer videoplayer = GetComponent<VideoPlayer>();
+
+        videoplayer.source = VideoSource.Url;
+        videoplayer.url = Application.streamingAssetsPath + "/" + relativePath;
+        videoplayer.isLooping = true;
+
+        // 読込完了時のコールバック
+        videoplayer.prepareCompleted += PrepareCompleted;
+        videoplayer.Prepare();
+
+    }
+    void PrepareCompleted(VideoPlayer vp)
+    {
+        vp.prepareCompleted -= PrepareCompleted;
+        vp.Play();
+	}
+
+    public void VideoPlayerControl()
+    {
+        VideoPlayer videoplayer = GetComponent<VideoPlayer>();
+
+        if(!videoplayer.isPlaying) {
+            videoplayer.Play();
+        } else {
+            videoplayer.Pause();
+        }
+    }
+
+}
